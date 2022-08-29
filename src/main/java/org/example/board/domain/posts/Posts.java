d90ff73ee9f9dded1743abfee3f840856b5da0b5
sponbob-pat <ixdos1111@gmail.com>
2022-08-29 21:33:32 +0900

    Entity 생성

---
+++ src/main/java/org/example/board/domain/posts/Posts.java
@@ -0,0 +1,47 @@
+package org.example.board.domain.posts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// Entity에서는 setter를 만들면 안됨
+// 왜냐면, 해당 클래스의 인스턴스 값들이 언제 어디서 변해야 하는지 코드상으로 구분할 수 없기 때문
+@Getter // lombok 어노테이션 - 빌더 패턴 클래스 생성
+@NoArgsConstructor // lombok 어노테이션 - 기본 생성자 자동 추가
+@Entity // JPA 어노테이션 - Posts 클래스는 실제 DB 클래스와 매칭할 클래스 = Entity
+public class Posts {
+
+    @Id // 해당 테이블의 Primary Key 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙 - IDENTITY를 통해 auto_increment 하도록 했음
+    private Long id;
+
+    // Column 어노테이션이 없어도 컬럼이 된다.
+    // 아래와 같이 자료형이나 길이를 설정하고 싶을 때 사용하면 된다.
+    @Column(length = 500, nullable = false)
+    private String title; // 글 제목
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content; // 글 내용
+
+    private String author; // 글 저자
+
+    // Builder 클래스를 통해 다음과 같은 작업 가능
+    // Posts.builder()
+    //      .title(title)
+    //      .content(content)
+    //      .author(author)
+    //      .build();
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ src/test/java/org/example/board/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,55 @@
+package org.example.board.domain.posts;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 별다른 설정이 없다면 h2 데이터베이스를 자동으로 실행함
+// 여기서 save와 findAll 기능을 테스트하겠다.
+public class PostsRepositoryTest {
+
+    // 스프링이 관리하는 빈(Bean)을 주입 받음
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // 테스트 이후에 실행될 메소드
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void post_load() {
+
+        // given
+        String title = "테스트 게시글";
+        String content = "게시글 본문";
+        String author = "김가루";
+
+        // 주어진 글 저장
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll(); // 테이블 posts에 있는 모든 데이터 조회
+
+        // then
+        Posts posts = postsList.get(0); // 0번째에 있는 값
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+
+    }
+
+
+}
